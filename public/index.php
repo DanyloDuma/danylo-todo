7d1ac54b0c6b19810b78c8cbdcfd5cbec9d8b0bb
DanyloDuma <danyloduma06@gmail.com>
2026-05-12 17:43:05 +0100

    feat: adição de includes e clean code

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-PT">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+
+  <title>Home - Danylo-ToDo</title>
+</head>
+
+
+
+<body>
+  <?php require 'header.php'; ?>
+
+  <p>Caminho secreto: <a href="experimentos.html">Aqui</a></p>
+
+  <?php require 'footer.php'; ?>
+</body>
+
+</html>
