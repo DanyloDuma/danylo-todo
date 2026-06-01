84e8dc2abca86b0bc972cc228a68e72bcfb0ae18
DanyloDuma <danyloduma06@gmail.com>
2026-06-01 17:45:03 +0100

    feat: Criação dos componentes home, sobre, contato e login

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { ContatoComponent } from './components/contato/contato.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  //{ path: '', component: AboutComponent },
-  //{ path: 'about', component: AboutComponent },
-  //{ path: 'contact', component: ContactComponent },
-  //{ path: 'home', component: HomeComponent },
-  //{ path: 'news', component: NewsComponent },
+  { path: '', component: HomeComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: 'contato', component: ContatoComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,9 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { ContatoComponent } from './components/contato/contato.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, SobreComponent, ContatoComponent, LoginComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent],
